3f81a29ce8b0fc55977a38b19ddcf1c1b09f454f
Peter Pistorius <peter.pistorius@gmail.com>
2021-01-28 17:38:42 +0200

    Remove reliance on library.

--- packages/auth/src/authClients/azureActiveDirectory.ts
+++ packages/auth/src/authClients/azureActiveDirectory.ts
@@ -1,4 +1,4 @@
-import { UserAgentApplication as AzureActiveDirectory } from 'msal'
+import type { UserAgentApplication as AzureActiveDirectory } from 'msal'
 
 export type { AzureActiveDirectory }
 import type { AuthClient } from './'
@@ -15,7 +15,24 @@ export const azureActiveDirectory = (
     login: async (options?) => await client.loginPopup(options),
     logout: (options?) => client.logout(options),
     signup: async (options?) => await client.loginPopup(options),
-    getToken: async () => sessionStorage.getItem('msal.idtoken'),
+    getToken: async (options?: any) => {
+      const renewIdTokenRequest = options || {
+        scopes: [process.env.AZURE_ACTIVE_DIRECTORY_CLIENT_ID],
+      }
+
+      try {
+        const response = await client.acquireTokenSilent(renewIdTokenRequest)
+        return response?.idToken?.rawIdToken || null
+      } catch (error) {
+        if (error.name === 'InteractionRequiredAuthError') {
+          client.acquireTokenRedirect(renewIdTokenRequest)
+        } else {
+          console.error(`azureActiveDirectory: Uncaught exception`, error)
+        }
+      }
+
+      return null
+    },
     getUserMetadata: async () => (await client.getAccount()) || null,
   }
 }
